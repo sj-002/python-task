717417c2f71264e66e6a403f5af72dc600baaf44
sj-002 <suriyashangar@gmail.com>
2025-01-16 13:32:45 +0530

    change

--- loop.py
+++ loop.py
@@ -47,3 +47,32 @@
 # for i in x:
 #     if(i>10):
 #         print(i)
+
+        #prime b/t 10 and 20
+# for i in range(10,21):
+#         if(i%2!=0 and i%3!=0):
+#                 print(i)
+
+        #largest num in list
+# x=[2,1,5,4,3]
+# largest_number=x[0]
+# for num in x:
+#         if num>largest_number:
+#                 largest_number=num
+# print('largest number is:',largest_number)
+
+        #num of vowel in str
+# str='letter'
+# count=0
+# for x in str:
+#         if x=='a' or x=='e' or x=='i' or x=='o' or x=='u':
+#                 print(x)
+#                 count+=1
+# print(count)
+
+        #prod from 1 to 5
+# prod=1 
+# for i in range(1,6):
+#         prod*=i
+# print(prod)
+        
