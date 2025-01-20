7acd65daa021bffbe22cd585d4a27c6e4f223104
sj-002 <suriyashangar@gmail.com>
2025-01-20 11:14:08 +0530

    mod change

---
+++ pattern.py
@@ -0,0 +1,15 @@
+# x=5
+# for i in range(x):
+#     for j in range(0,5):
+#         print(x-j,end=' ')
+#     print()
+
+
+# for i in range(1,50):
+#     if(i%2!=0):
+#         print(i,end=' ')
+
+# n = 5  
+# for i in range(1, n + 1):
+#     print('1'*i) 
+
