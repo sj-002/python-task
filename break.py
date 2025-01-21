3dde7148e9b293602acce30059448f430a5c7129
sj-002 <suriyashangar@gmail.com>
2025-01-21 12:56:06 +0530

    mod 1

--- break.py
+++ break.py
@@ -55,3 +55,35 @@
 # list=[1,-2,-1,3,4,0]
 # for i in list:
 #     pass
+
+        #10 1 to 10, skip 5 and stop at 8
+# for i in range(1,11):
+#         if(i==5):
+#                 continue
+#         if(i==8):
+#                 break
+#         print(i)
+
+        #11 odd numbers from 1 to 10,use pass for even numbers
+# for i in range(1,11):
+#         if(i%2!=0):
+#                 print(i)
+#         if(i%2==0):
+#                 pass
+
+        #12 Iterate over a list, skip negatives, print positives, stop at zero.
+# list=[1,-2,-1,3,4,0,5]
+# for i in list:
+#         if(i<0):
+#                 continue
+#         if(i==0):
+#                 break
+#         print(i)
+
+# list=[1,-2,-1,3,4,5,2]
+# mid=len(list)//2
+# for i in list:
+#         if (i<mid):
+#                 continue
+#         print(i)        
+
