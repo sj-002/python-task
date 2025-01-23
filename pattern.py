8e77a7ea4f17ff60c07d207d86054a857ec61c4d
sj-002 <suriyashangar@gmail.com>
2025-01-23 12:32:21 +0530

    set mod

--- pattern.py
+++ pattern.py
@@ -8,24 +8,31 @@
 
 # for i in range(1,50):
 #     if(i%2!=0):
-#         print(i,end=' ')
+#         print(i)
 
+        #34
 # n = 5  
 # for i in range(1, n + 1):
-#     print('a'*i) 
+#     print('*'*i) 
 
+        #38
 # for i in range(1,6):
 #     for j in range(1,i+1):
 #         print(6-j,end='')
 #     print()
     
+        #37
 # for i in range(1,6):
 #     for j in range(1,i+1):
 #         print(6-i,end='')
 #     print()
 
-# letter='abcde'
+        #27
+# letter='A B C D E'
 # for i in range(1,6):
 #         print(letter)
 
 
+# n = 5  
+# for i in range(n):
+#     print(i+1,i) 
