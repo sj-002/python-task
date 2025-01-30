a32546fb9d807ef0176bec6699fb1e06fae6899c
sj-002 <suriyashangar@gmail.com>
2025-01-30 12:44:19 +0530

    fix

--- kwargs.py
+++ kwargs.py
@@ -52,4 +52,31 @@
 # x=lambda a:a*a
 # print(x(15))
 
+    #1 RECURSION
+# def recursion(k):
+#     for i in range(k):
+#         i=i+1
+#         print(i)
+# recursion(10)
 
+    #2 SUM OF LIST
+# def recursion(*k):
+#     sum=0
+#     for i in k:
+#      sum=sum+i
+#     print('sum is:',sum)
+# recursion(2,4,6,8,10)
+
+
+
+    #5 GET INPUT AND PRINT FACT
+# import math
+# def recursion(x):
+#     if x<0:
+#         print('invalid')
+#     elif x==0:
+#         print('factorial of zero is:',1)
+#     else:
+#         print('factorial of a number is:', math.factorial(x))
+# number=(int(input('enter number:')))
+# recursion(number)
