1787b924ee8c0ec0c053429801d24e70ce322954
sj-002 <suriyashangar@gmail.com>
2025-02-18 12:26:05 +0530

    fix

---
+++ class.py
@@ -0,0 +1,23 @@
+# class bank:
+#     counter1="Loansection"
+#     counter2="Depositsection"
+#     counter3="Manager"
+
+#     def loan(self):
+#         print('loan section is counter 1')
+#     def deposit(self):
+#         print('deposit section is counter 2')
+#     def manager(self):
+#           print('manger section is counter 3')
+# axis=bank()
+# axis.loan()
+# axis.deposit()
+# axis.manager()
+
+# sbi=bank()
+# a=sbi.loan=('for loan counter 2')
+# print(a)
+
+
+
+    
